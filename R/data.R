053b6cc65842e069ae8ef3c08f57e67b0724d2e0
Ethan Plunkett <ethan@plunkettworks.com>
2023-01-30 16:50:41 -0500

    Added amewoo example dataset

---
+++ R/data.R
@@ -0,0 +1,31 @@
+# This file documents data
+
+#' @name BirdFlowModels
+#' @aliases BirdFlowModels
+#' @aliases amewoo
+#' @title Birdflow Example Models
+#' @rdname BirdFlowModels
+#' @description This BirdFlow model is intended for for testing code and
+#'   demonstrating usage in \pkg{BirdFlowR}; it is not fully vetted and should
+#'   not be used for research or inference.
+#'
+#'   Currently there is just one model "amewoo" (American woodcock) which lacks
+#'   complete metadata. It will likely be replaced soon.
+#'
+#' @format A BirdFlow model
+#'
+#' @source Trained on eBird Status and Trends 2019 Data.
+#'
+#' @references Fink, D., T. Auer, A. Johnston, M. Strimas-Mackey, S. Ligocki, O.
+#' Robinson, W. Hochachka, L. Jaromczyk, A. Rodewald, C. Wood, I. Davies, A.
+#' Spencer. 2022. eBird Status and Trends, Data Version: 2021; Released: 2022.
+#' Cornell Lab of Ornithology, Ithaca, New York.
+#' https://doi.org/10.2173/ebirdst.2021
+#'
+#' @usage amewoo
+#' @examples
+#' print(amewoo)
+#' species(amewoo)
+#' @export
+"amewoo"
+
--- README.md
+++ README.md
@@ -3,3 +3,10 @@ Contains prototype and example BirdFlowR models
 
 This is a "Data" R package that contains fitted BirdFlow models for use with the BirdFlowR package.  The models in this package are currently prototypes intended for testing and demonstrating the code.  They have not been vetted for scientific use. 
 
+## Installation
+
+Use the code below to install from github
+
+```{r}
+devtools::install_github("birdflow-science/BirdFlowModels")
+```
